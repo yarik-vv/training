7653663c23f93499c1cdfaecede06896185ed2fb
Yarik Vashchuk <yarik.vashchuk@gmail.com>
2017-03-06 22:50:08 +0200

    delete server.js (use webpack dev server)

--- server.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var development = new static.Server('./webpack/public');
-
-require('http').createServer(function (request, response) {
-  if (!/\./.test(request.url)) {
-    request.url = '/';
-  }
-  development.serve(request, response);
-}).listen(3000);
