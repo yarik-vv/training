71a62dc9a03a3af4231bbe77160d994f74dd1937
Your Name <yarik.vashchuk@gmail.com>
2017-03-08 22:38:51 +0200

    nodeJS: modules

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,10 +1,4 @@
-function User(name){
-    this.name = name;
-}
-
-User.prototype.hello = function(who){
-    console.log("Hello, " + who.name);
-};
+require('./user');
 
 var vasya = new user.User("Вася");
 var petya = new user.User("Петя");
