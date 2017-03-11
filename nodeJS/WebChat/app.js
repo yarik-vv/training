c0e2a073f3b477f0f2dad95d7158dd101e9ef88e
Your Name <yarik.vashchuk@gmail.com>
2017-03-11 05:24:09 +0200

    nodeJS: loger, config

--- nodeJS/WebChat/app.js
+++ nodeJS/WebChat/app.js
@@ -1,43 +1,39 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
 
 var app = express();
-app.set('port', 3000);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.set('views', __dirname + '/template');
+app.set('view engine', 'ejs');
+
+app.use(express.favicon()); // /favicon.ico
+
+if (app.get('env') == 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+
+app.use(express.bodyParser());  // req.body....
+
+app.use(express.cookieParser()); // req.cookies
+
+app.use(app.router);
+
+app.get('/', function(req, res, next) {
+  res.render("index", {
+    body: '<b>Hello</b>'
+  });
 });
 
-app.use(function(req, res, next) {
-  if (req.url == '/') {
-    res.end("Hello");
-  } else {
-    next();
-  }
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  if (req.url == '/forbidden') {
-    next(new Error("wops, denied"));
-  } else {
-    next();
-  }
-});
-
-app.use(function(req, res, next) {
-  if (req.url == '/test') {
-    res.end("Test");
-  } else {
-    next();
-  }
-});
-
-app.use(function(req, res) {
-  res.send(404, "Page Not Found, sorry");
-});
 
 app.use(function(err, req, res, next) {
+  // NODE_ENV = 'production'
   if (app.get('env') == 'development') {
     var errorHandler = express.errorHandler();
     errorHandler(err, req, res, next);
@@ -45,30 +41,18 @@ app.use(function(err, req, res, next) {
     res.send(500);
   }
 });
+/*
 
+var routes = require('./routes');
+var user = require('./routes/user');
 
+// all environments
 
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-// var routes = require('./routes');
-// var user = require('./routes/user');
+*/
 
-// // all environments
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(express.cookieParser('your secret here'));
-// app.use(express.session());
-// app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // development only
-// if ('development' == app.get('env')) {
-//   app.use(express.errorHandler());
-// }
-
-// app.get('/', routes.index);
-// app.get('/users', user.list);
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + config.get('port'));
+});
