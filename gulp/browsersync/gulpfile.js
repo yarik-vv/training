d10fe6458509e3521c18096817f7ffbc8261d791
Your Name <yarik.vashchuk@gmail.com>
2017-03-20 19:02:58 +0200

    Gulp: browser-sync

---
+++ gulp/browsersync/gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const del = require('del');
+const concatCSS = require('gulp-concat-css');
+const debug = require('gulp-debug');
+const browserSync = require('browser-sync').create();
+
+//const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+gulp.task('css', function() {
+  return gulp.src('frontend/**/index.css')
+    .pipe(debug({title: 'src'}))
+    .pipe(concatCSS('css/main.css'))
+    .pipe(debug({title: 'concatCSS'}))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('clean', function() {
+  return del('public');
+});
+
+gulp.task('files', function() {
+  return gulp.src('frontend/assets/**')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series(
+    'clean',
+    gulp.parallel('css', 'files'))
+);
+
+gulp.task('watch', function() {
+  gulp.watch('frontend/styles/**/*.*', gulp.series('css'));
+
+  gulp.watch('frontend/assets/**/*.*', gulp.series('files'));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: 'public'
+  });
+
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', 
+  gulp.series(
+    'build', 
+    gulp.parallel('watch', 'serve')
+  )
+);
