76e1835110dce9d00a60ce4e2ee687ddeaab0f8b
Your Name <yarik.vashchuk@gmail.com>
2017-03-20 20:47:37 +0200

    Gulp: create plugins with through2

---
+++ gulp/plugins-through2/gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const gulp = require('gulp');
+const through2 = require('through2').obj;
+const File = require('vinyl');
+
+gulp.task('manifest', function() {
+
+  const mtimes = {};
+
+  return gulp.src('frontend/**/*.*')
+      .pipe(through2(
+          function(file, enc, callback) {
+            mtimes[file.relative] = file.stat.mtime;
+            callback(null, file);
+          }
+      ))
+      .pipe(gulp.dest('public'))
+      .pipe(through2(
+          function(file, enc, callback) {
+            callback();
+          },
+          function(callback) {
+            let manifest = new File({
+              // cwd base path contents
+              contents: new Buffer(JSON.stringify(mtimes)),
+              base:     process.cwd(),
+              path:     process.cwd() + '/manifest.json'
+            });
+            this.push(manifest);
+            callback();
+          }
+      ))
+      .pipe(gulp.dest('.'));
+});
