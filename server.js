c18522a13f8e078f8599f651b5e92654c19b427c
Your Name <yarik.vashchuk@gmail.com>
2017-03-22 19:49:39 +0200

    Gulp: part integration with webpack

---
+++ server.js
@@ -0,0 +1,9 @@
+var static = require('node-static');
+var development = new static.Server('./gulp/gulp+webpack/public');
+
+require('http').createServer(function (request, response) {
+  if (!/\./.test(request.url)) {
+    request.url = '/';
+  }
+  development.serve(request, response);
+}).listen(3000);
